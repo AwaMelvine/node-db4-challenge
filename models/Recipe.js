f871bc1ba6a87a58c73f04437c56398b54adcd08
Awa Melvine <melvineawa@gmail.com>
2019-07-18 15:32:33 +0100

    Create model method to get recipe instructions

--- models/Recipe.js
+++ models/Recipe.js
@@ -6,12 +6,21 @@ module.exports = {
     },
 
     async getShoppingList(recipe_id) {
-        const query = await db.select('ingredients.id', 'ingredients.name', 'ingredients.quantity')
+        const list = await db.select('ingredients.id', 'ingredients.name', 'ingredients.quantity')
             .from('ingredients')
             .innerJoin('recipe_ingredient', 'recipe_ingredient.ingredient_id', 'ingredients.id')
             .innerJoin('recipes', 'recipe_ingredient.recipe_id', 'recipes.id')
             .where('recipe_ingredient.recipe_id', recipe_id);
-        return query;
+        return list;
+    },
+
+    async getInstructions(recipe_id) {
+        const instructions = await db.select('instructions.id', 'instructions.step_number', 'instructions.text')
+            .from('instructions')
+            .innerJoin('recipes', 'instructions.recipe_id', 'recipes.id')
+            .where('recipes.id', recipe_id)
+            .orderBy('instructions.step_number', 'asc');
+        return instructions;
     },
 
     async findById(id = null) {

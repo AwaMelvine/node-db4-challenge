2581c3867b293a09f1849ecc0ca09fa8ce1a72ac
Awa Melvine <melvineawa@gmail.com>
2019-07-18 14:21:53 +0100

    Create model methods to update and delete recipe

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -14,4 +14,16 @@ module.exports = {
         return this.get(id);
     },
 
+    async update(changes, id) {
+        const count = await db('recipes').where('id', id).update(changes);
+        return (count > 0 ? this.get(id) : null);
+    },
+
+    async remove(id) {
+        const count = await db('recipes').where('id', id).del();
+        return count;
+    }
+
+
+
 };

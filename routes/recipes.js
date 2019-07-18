fd99f15dbb47e2e41ccc2758a97925a3480ffa13
Awa Melvine <melvineawa@gmail.com>
2019-07-18 15:45:34 +0100

    Create endpoint to get shopping list

--- routes/recipes.js
+++ routes/recipes.js
@@ -12,4 +12,14 @@ router.get('/', async (req, res) => {
 });
 
 
+router.get('/:id/shoppingList', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const list = await Recipe.getShoppingList(id);
+        res.status(200).json({ data: list });
+    } catch (error) {
+        res.status(500).json({ error: "Oops... there was a server error" });
+    }
+});
+
 module.exports = router;
